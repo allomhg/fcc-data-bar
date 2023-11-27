059221a232e160ac7e121f5f0e792dd038234296
allomhg <allomhg@gmail.com>
2023-11-27 21:06:57 +0000

    Playing around

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-var hello = 1;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createBarGraph = void 0;
+var d3 = require("d3");
+function createBarGraph() {
+    var height = 500;
+    var width = 500;
+    var margin = 30;
+    console.log("hello");
+    var svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
+}
+exports.createBarGraph = createBarGraph;
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,1 +1,15 @@
-const ads = 1;
+import * as d3 from "d3";
+
+export function createBarGraph() {
+    const height = 500;
+    const width = 500;
+    const margin = 30;
+    
+    console.log("hello");
+
+    const svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    
+}
